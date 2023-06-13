700ea853ba1ddb8e975b7001d60ed5f318ae8db5
Uriel Ortega <urielortega2509@gmail.com>
2023-06-13 12:06:56 -0600

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Uriel Ortega on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
