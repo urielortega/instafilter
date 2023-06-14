7e20b6ae484e92fdd1cfed58f9f1616a51f40c7d
Uriel Ortega <urielortega2509@gmail.com>
2023-06-14 11:27:51 -0600

    Implemented images saving to user’s photo library.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -21,6 +21,13 @@ struct ContentView: View {
             Button("Select Image") {
                 showingImagePicker = true
             }
+            
+            Button("Save Image") {
+                guard let inputImage = inputImage else { return }
+                
+                let imageSaver = ImageSaver()
+                imageSaver.writeToPhotoAlbum(image: inputImage)
+            }
         }
         .padding()
         .sheet(isPresented: $showingImagePicker) {
